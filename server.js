806303091a7273938c300f20e1a1202e2403db10
Doug Elkin <doug@dougelkin.com>
2020-08-29 00:34:18 -0400

    :heavy_plus_sign::lock: authentication
    - lock down media directory

--- server.js
+++ server.js
@@ -1,23 +1,78 @@
 #!/bin/env node
 
+const bodyParser = require('body-parser')
+const fs = require('fs')
 const path = require('path')
 const { exec } = require('child_process')
 const express = require('express')
+const bcrypt = require('bcryptjs')
+const session = require('express-session')
+const MemoryStore = require('memorystore')(session)
 const { urlValidator } = require('@preaction/validation')
 
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 
-app.use('/media', express.static(path.join(__dirname, 'media')))
+const sessionConfig = {
+  secret: String(+new Date()) + String(Math.random()),
+  name: 'warp-audio-session',
+  cookie: {
+    sameSite: 'strict',
+    secure: process.env.WARP_COOKIE_SECURE,
+  },
+  store: new MemoryStore({
+    checkPeriod: 24 * 60 * 60 * 1000, // un dia
+  }),
+  proxy: !!process.env.WARP_COOKIE_PROXY,
+  resave: false,
+  saveUninitialized: false,
+}
+
+const Session = session(sessionConfig)
+app.use(Session)
+app.use(bodyParser.json())
+
+const authenticationRequired = (req, res, next) => {
+  if (!req.session.authenticated) {
+    return res.status(401).send('')
+  }
+  next()
+}
+
+app.use(
+  '/media',
+  authenticationRequired,
+  express.static(path.join(__dirname, 'media'))
+)
 app.use('/', express.static(path.join(__dirname, 'build')))
+app
+  .route('/api/session')
+  .get((req, res) => {
+    res.json(req.session)
+  })
+  .post((req, res) => {
+    if (bcrypt.compareSync(req.body.pw, pw)) {
+      req.session.authenticated = true
+      res.json(req.session)
+    } else {
+      res.status(401).json({})
+    }
+  })
+
+io.use((socket, next) => {
+  Session(socket.request, socket.request.res, next)
+})
 
 io.on('connection', (socket) => {
   socket.on('warp-request', (state, fn) => {
-    fn()
+    if (!socket.conn.request.session.authenticated) {
+      return
+    }
     if (urlValidator(state.url) || !state.url) {
       return
     }
+    fn()
     const url = state.url
     const shiftPitch = state.shiftPitch || '0'
     const stretchTempo = state.stretchTempo || '1'
@@ -42,6 +97,17 @@ io.on('connection', (socket) => {
     )
   })
 })
+
+let pw
+try {
+  pw = fs.readFileSync(path.resolve(__dirname, '.pw'), 'utf-8')
+} catch (e) {
+  pw = String(+new Date()) + String(Math.random())
+  const salt = bcrypt.genSaltSync()
+  console.log(`pw not set. setting to: ${pw}`)
+  pw = bcrypt.hashSync(pw, salt)
+  console.log('to set, execute: npm run set-password')
+}
 
 const port = process.env.PORT || 8712
 http.listen(port, () => {
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import axios from 'axios'
 import React from 'react'
 import ReactPlayer from 'react-player'
 import io from 'socket.io-client'
-import { AnimatedButton, Boilerplate } from '@preaction/bootstrap-clips'
-import { Input } from '@preaction/inputs/dist/preaction-inputs.cjs.js'
+import {
+  AnimatedButton,
+  Boilerplate,
+  Spinner,
+} from '@preaction/bootstrap-clips'
+import { Input, Form } from '@preaction/inputs/dist/preaction-inputs.cjs.js'
 import { urlValidator } from '@preaction/validation'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'animate.css/animate.min.css'
@@ -13,6 +18,8 @@ class App extends React.Component {
     super(props)
     this.state = {
       audio: '',
+      authenticated: undefined,
+      loginFormPw: '',
       processing: false,
       shiftPitch: 0,
       stretchTempo: 1.0,
@@ -22,6 +29,10 @@ class App extends React.Component {
 
   componentDidMount() {
     this.socket = io({ path: '/socket.io' })
+    axios.get('/api/session').then((response) => {
+      const authenticated = !!response.data.authenticated
+      this.setState({ authenticated })
+    })
     this.socket.on('warp-complete', (state) => {
       this.setState({
         processing: false,
@@ -68,85 +79,138 @@ class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <Boilerplate
-          jumbotron={
-            <div>
-              <h1 className='mb-3'>Warp Audio Server</h1>
-              <Input
-                type='url'
-                onInput={(event) => {
-                  event.target.blur()
-                }}
-                placeholder='URL'
-                required
-                value={this.state.url}
-                valueHandler={this.getValueHandler('url')}
-              />
-              <button
-                type='button'
-                className='btn btn-lg btn-primary mb-3'
-                onClick={async () => {
-                  this.setState({ url: '' })
-                }}
-              >
-                Clear
-              </button>
-            </div>
-          }
-        >
-          <Input
-            label='Shift Pitch'
-            type='number'
-            min='-12'
-            max='12'
-            value={this.state.shiftPitch}
-            valueHandler={this.getValueHandler('shiftPitch')}
-          />
-          <Input
-            type='range'
-            min='-12'
-            max='12'
-            value={this.state.shiftPitch}
-            valueHandler={this.getValueHandler('shiftPitch')}
-          />
-          <Input
-            type='number'
-            label='Stretch Tempo'
-            min='0.00'
-            max='2.00'
-            step='0.01'
-            value={this.state.stretchTempo}
-            valueHandler={this.getValueHandler('stretchTempo')}
-          />
-          <Input
-            type='range'
-            min='0.00'
-            max='2.00'
-            step='0.01'
-            value={this.state.stretchTempo}
-            valueHandler={this.getValueHandler('stretchTempo')}
-          />
-          <hr />
-          <div className='btn-group mb-3'>
-            <AnimatedButton
-              disabled={this.state.processing}
-              onClick={this.submit.bind(this)}
-            >
-              Warp
-            </AnimatedButton>
+        {this.state.authenticated === undefined ? (
+          <div
+            style={{
+              width: '10rem',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              marginTop: '2rem',
+            }}
+          >
+            <Spinner type='grow' size='10' />
           </div>
-          {this.state.audio ? (
-            <ReactPlayer
-              url={this.state.audio}
-              controls
-              width='100%'
-              height='3em'
-              playing
+        ) : (
+          ''
+        )}
+        {this.state.authenticated === true ? (
+          <Boilerplate
+            jumbotron={
+              <div>
+                <h1 className='mb-3'>Warp Audio Server</h1>
+                <Input
+                  type='url'
+                  placeholder='URL'
+                  required
+                  value={this.state.url}
+                  valueHandler={this.getValueHandler('url')}
+                />
+                <button
+                  type='button'
+                  className='btn btn-lg btn-primary mb-3'
+                  onClick={async () => {
+                    this.setState({ url: '' })
+                  }}
+                >
+                  Clear
+                </button>
+              </div>
+            }
+          >
+            <Input
+              label='Shift Pitch'
+              type='number'
+              min='-12'
+              max='12'
+              value={this.state.shiftPitch}
+              valueHandler={this.getValueHandler('shiftPitch')}
             />
-          ) : (
-            ''
-          )}
-        </Boilerplate>
+            <Input
+              type='range'
+              min='-12'
+              max='12'
+              value={this.state.shiftPitch}
+              valueHandler={this.getValueHandler('shiftPitch')}
+            />
+            <Input
+              type='number'
+              label='Stretch Tempo'
+              min='0.00'
+              max='2.00'
+              step='0.01'
+              value={this.state.stretchTempo}
+              valueHandler={this.getValueHandler('stretchTempo')}
+            />
+            <Input
+              type='range'
+              min='0.00'
+              max='2.00'
+              step='0.01'
+              value={this.state.stretchTempo}
+              valueHandler={this.getValueHandler('stretchTempo')}
+            />
+            <hr />
+            <div className='btn-group mb-3'>
+              <AnimatedButton
+                disabled={this.state.processing}
+                onClick={this.submit.bind(this)}
+              >
+                Warp
+              </AnimatedButton>
+            </div>
+            {this.state.audio ? (
+              <ReactPlayer
+                url={this.state.audio}
+                controls
+                width='100%'
+                height='3em'
+                playing
+              />
+            ) : (
+              ''
+            )}
+          </Boilerplate>
+        ) : (
+          ''
+        )}
+        {this.state.authenticated === false ? (
+          <Boilerplate
+            jumbotron={<h1 className='display-4'>Warp Audio Server Login</h1>}
+          >
+            <div className='row'>
+              <div className='col-lg-6 ml-auto mr-auto'>
+                <Form
+                  noValidate
+                  onSubmit={(e) => {
+                    e.preventDefault()
+                    axios
+                      .post('/api/session', { pw: this.state.loginFormPw })
+                      .then((response) => {
+                        const authenticated = !!response.data.authenticated
+                        this.setState({ authenticated })
+                      })
+                  }}
+                >
+                  <Input
+                    name='password'
+                    label='Password'
+                    type='password'
+                    required
+                    value={this.state.loginFormPw}
+                    valueHandler={(value) => {
+                      this.setState({ loginFormPw: value })
+                    }}
+                  />
+                  <button type='submit' className='btn btn-primary'>
+                    Login
+                  </button>
+                </Form>
+              </div>
+            </div>
+          </Boilerplate>
+        ) : (
+          ''
+        )}
       </div>
     )
   }
